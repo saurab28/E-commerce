c00bab8ab15c907af84e1cd41f303f5e54e1b2e9
saurab28 <sarafsaurab24@gmail.com>
2025-09-12 14:22:28 +0530

    adding-router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,21 @@
+import Home from '@/views/Home.vue'
+import Login from '@/views/Login.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path:"/",
+      name:"home",
+      component:Home
+    },
+    {
+      path:"/login",
+      name:"Login",
+      component: Login
+    }
+  ],
 })
 
 export default router
