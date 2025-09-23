032b00884a88cea9a0692d950337c21f3a4fde54
saurab28 <sarafsaurab24@gmail.com>
2025-09-23 11:27:36 +0530

    added-changes

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -11,7 +11,7 @@ const Category = ref<ICategorycard[]>([])
 const allProducts = ref<ICard[]>([])
 const props = defineProps<{ selectedCategory?: string }>()
 watch(
-  () => props.selectedCategory, 
+  () => props.selectedCategory,
   () => {
     const filterProducts = Category.value.filter((eachCategory: ICategorycard) => {
       if (props.selectedCategory === 'Sweet Tooth') {
@@ -64,7 +64,7 @@ const loading = ref(true)
   </div>
   <div v-else-if="Productstore.error">{{ Productstore.error }}</div>
   <div class="container mx-auto px-[104px]">
-    <div class="grid gap-6 grid-cols-4
+    <div class="grid gap-6 grid-cols-5
     ">
       <ProductCard
         v-for="eachProductcard in allProducts"
